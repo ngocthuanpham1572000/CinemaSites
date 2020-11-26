222dd0117e2414b13b73c32891cb2bf3674106ba
ngocthuanpham1572000 gmail.com <ngocthuanpham1572000@gmail.com>
2020-11-26 14:26:09 +0700

    Add project files.

---
+++ Cinema/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
