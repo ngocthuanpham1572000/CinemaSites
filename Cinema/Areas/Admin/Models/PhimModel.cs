b1c69df6d1e733f90a0a9a68b77ba0da615fc809
toan01693074766 <toan01693074766@gmail.com>
2020-11-27 11:00:25 +0700

    Loai và phim

---
+++ Cinema/Areas/Admin/Models/PhimModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Cinema.Areas.Admin.Models
+{
+    public class PhimModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =3)]
+        public string TenPhim { get; set; }
+        [Required]
+        public int ThoiLuong { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string DaoDien { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string DienVien { get; set; }
+        [StringLength(50, MinimumLength = 2)]
+        public string QuocGia { get; set; }
+        [StringLength(1000, MinimumLength = 3)]
+        public string Mota { get; set; }
+        [Required]
+        public string HinhAnh { get; set; }
+
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime NgayPhatHanh { get; set; }
+        [Required]
+        public int LoaiPhim { get; set; }
+        [ForeignKey("LoaiPhim")]
+        public virtual LoaiPhimModel Loai { get; set; }
+
+    }
+}
