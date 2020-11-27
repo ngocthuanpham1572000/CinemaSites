fea06bccaaa9f3847c0e8e1d3d572d98e8f24afd
ngocthuanpham1572000 gmail.com <ngocthuanpham1572000@gmail.com>
2020-11-27 10:49:52 +0700

    PhongModel,RapPhimModel, CumRapModel

---
+++ Cinema/Areas/Admin/Models/PhongModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinema.Areas.Admin.Models
+{
+    public class PhongModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
+        public int TenPhong { get; set; }
+
+        public int TrangThai { get; set; }
+        public int Rap { get; set; }
+        [ForeignKey("Rap")]
+        public virtual RapPhimModel MaRapPhim { get; set; }
+        public ICollection<HangModel> LstHang { get; set; }
+        public ICollection<SuatChieuModel> LstSuatChieu { get; set; }
+
+    }
+}
---
+++ Cinema/Areas/Admin/Models/RapPhimModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Cinema.Areas.Admin.Models
+{
+    public class RapPhimModel
+    {
+        public int Id { get; set; }
+       
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
+        public string TenRap { get; set; }
+
+        [StringLength(200, MinimumLength = 3)]
+        [Required]
+        public string DiaChi { get; set; }
+        [Required]
+        public int CumRap { get; set; }
+        [ForeignKey("CumRap")]
+        public virtual CumRapModel MaCumRap { get; set; }
+
+        [Required]
+        public int TrangThai { get; set; }
+
+        public ICollection<PhongModel> LstPhong { get; set; }
+        
+       
+
+        
+    }
+}
